ba9947de81346544437dad960b7c493f98f1d44d
mr-exception <drabandi1996@gmail.com>
2017-11-07 02:17:32 +0330

    on text hooks developed

--- libs/telegram.js
+++ libs/telegram.js
@@ -1,17 +1,24 @@
 var request = require("request");
 
 var token = 'NA'
+var text_messages_splitter = ' '
 
 var set_token = (bot_token) => {
   token = bot_token
+}
+
+var set_text_messages_splitter = (splitter) => {
+  text_messages_splitter = splitter
 }
 
 var get_me = (success, fail) => {
   send_request('getMe', {}, (response) => {
     if(response.ok)
       success(response.result)
-    else
+    else{
+      console.log(response)
       fail()
+    }
   })
 }
 
@@ -42,9 +49,62 @@ var send_request = (method, body, on_success) => {
   })
 }
 
+var start = () => {
+  get_me(
+    // success
+    (info) => {
+      console.log(`connected to bot ${info.username}`)
+    }, () => {
+      console.log('can not connect to bot. check your internet connectivity or token')
+    }
+  )
+  
+  var last_update_id = 0
+  var check_updates = () => {
+    get_updates(last_update_id,
+      (messages) => {
+        messages.forEach((message) => {
+          if(last_update_id <= message.update_id){
+            last_update_id = message.update_id+1
+          }
+          if('text' in message.message)
+            got_text_message(message.message.text, message.message.chat.first_name, message.message.chat.username, message)
+        })
+      }, () => {
+        console.log('error in checking for updates')
+      }
+    )
+  }
+  setInterval(check_updates, 2000)
+}
+
+/*
+    text message callbacks
+*/
+var text_message_hooks = {}
+
+var got_text_message = (text, name, username, message) => {
+  var parts = text.split(text_messages_splitter)
+  var command = parts[0]
+  if(command in text_message_hooks){
+    parts.splice(0,1)
+    text_message_hooks[command].callback(parts, name, username, message)
+  }
+  else
+  console.log(`${message.update_id}#${username}: ${text}`)
+}
+
+var on_text = (command_string, callback, spliter=' ') =>{
+  text_message_hooks[command_string] = {callback, spliter}
+}
 module.exports = {
   set_token,
+  text_messages_splitter,
+  token,
+  
+  start,
 
+  on_text,
   get_me,
   get_updates,
 }
--- main.js
+++ main.js
@@ -3,3 +3,12 @@ const token = '460908339:AAGMTxi85BuwbHjZejRm7CyVoXFDRTocRtk'
 
 const telegram = require('./libs/telegram.js')
 telegram.set_token(token)
+telegram.start()
+
+telegram.on_text('/echo', (args, name, username, message) => {
+  console.log(`echo: ${args}`)
+})
+
+telegram.on_text('/repeat', (args, name, username, message) => {
+  console.log(`repeat#${name}: ${args}`)
+})
